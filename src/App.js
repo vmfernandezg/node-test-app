33bd46adc0fbcb3acc7f07ee9b7eb92c20dec852
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2020-02-16 17:53:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This is the new main page:
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
