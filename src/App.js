9bf0e2a71377ab25f270acbbc7db44011b3beb83
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2020-03-22 19:30:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the new MAIN PAGE:
+          This is the new MAIN PAGE OF THE APP:
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
